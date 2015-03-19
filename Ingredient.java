441daa5d05ae29187ad2e0d85b1788c523837968
Albie Brown <albert_brown@brown.edu>
2015-03-18 23:07:23 -0400

    Deleted

--- Ingredient.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.cookups;
-
-public interface Ingredient {
-	public Double ounces(); //amount in ounces
-	public Double teaspoons(); //amount in teaspoons
-	public Double expiration(); //seconds until expiration
-}
