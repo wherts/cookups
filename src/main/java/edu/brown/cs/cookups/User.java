3b9b6be9e1dccba7b611dafcf8ee08c103981340
Grant Gustafson <grant_gustafson@brown.edu>
2015-03-18 22:56:03 -0400

    new User.java

--- src/main/java/edu/brown/cs/cookups/User.java
+++ src/main/java/edu/brown/cs/cookups/User.java
@@ -1,3 +1,62 @@
 package edu.brown.cs.cookups;
 
+public interface User {
 
+	/**
+	  * Gets a User's ID.
+	  * @return id
+	  */
+	String id();
+
+	/**
+	  * Sets a User's ID.
+	  * @param id of user
+	  */
+	void setId(String id);
+
+	/**
+	  * Gets a User's name.
+	  * @return name
+	  */
+	String name();
+
+	/**
+	  * Sets a User's name.
+	  * @param name username
+	  */
+	void setName(String name);
+
+	/**
+	  * Gets a User's ingredients.
+	  * @return list of UserIngredients
+	  */
+	List<Ingredient> ingredients();
+
+	/**
+	  * Gets a user's schedule.
+	  * If a person is not active, return null.  
+	  * @return schedule
+	  */
+	Schedule schedule();
+
+	/**
+	  * Setss a user's schedule.
+	  * If a person is not active, set's null.  
+	  * @param schedule user's schedule
+	  */
+	void setSchedule(Schedule schedule);
+
+	/** 
+	  * Gets a User's location.
+	  * @return lat long
+	  */
+	LatLong location();
+
+	/** 
+	  * Get's a User's location.
+	  * @param location of user
+	  */
+	void setLocation(LatLong location);
+
+
+}
