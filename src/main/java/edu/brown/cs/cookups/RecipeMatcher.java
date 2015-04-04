13546cc756e9430f09f31fa10a681d67e3370bca
Wesley Herts <wesley_herts@brown.edu>
2015-04-04 11:05:27 -0400

    Added recipe finder

--- src/main/java/edu/brown/cs/cookups/RecipeMatcher.java
+++ src/main/java/edu/brown/cs/cookups/RecipeMatcher.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class RecipeMatcher {
   private Manager dbM;
@@ -16,6 +17,7 @@ public class RecipeMatcher {
   public List<Recipe> findRecipes(List<Person> chefs) {
     assert(chefs != null);
     numPeople = chefs.size();
+    //maps ingredient id to the amount in the group
     Map<String, Double> currIngredients = new HashMap<>();
     //1. compile ingredients
     compileIngredients(chefs, currIngredients);
@@ -35,8 +37,20 @@ public class RecipeMatcher {
     }
   }
 
-  public List<Recipe> matchRecipes(Map<String, Double> map) {
-    
-    return null;
+  public List<Recipe> matchRecipes(Map<String, Double> currIngredients, List<Recipe> recipes, int partySize) {
+    List<Recipe> myRecipes = new ArrayList<>();
+    for (Recipe recipe : recipes) {
+      for (RecipeIngredientProxy ing : recipe.ingredients()) {
+        if (currIngredients.containsKey(ing.id())) {
+          if (ing.ounces() > currIngredients.get(ing.id())) {
+            recipe.addToShoppingList(ing, ing.ounces() - currIngredients.get(ing.id()));
+          }
+        } else {
+          recipe.addToShoppingList(ing, ing.ounces());
+        }
+        myRecipes.add(recipe.scale(partySize));
+      }
+    }
+    return myRecipes;
   }
 }
