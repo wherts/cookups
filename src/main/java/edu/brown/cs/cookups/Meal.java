9d75fc7fe4aefe2c76734a4b44262690d560908d
Grant Gustafson <grant_gustafson@brown.edu>
2015-04-14 00:12:18 -0400

    db base

--- src/main/java/edu/brown/cs/cookups/Meal.java
+++ src/main/java/edu/brown/cs/cookups/Meal.java
@@ -3,10 +3,15 @@ package edu.brown.cs.cookups;
 import java.util.List;
 
 public interface Meal {
-	public double time();
-	public double duration();
-	public Location place();
-	public List<User> attendees();
-	public User host();
-	public Recipe recipe();
+  public double time();
+
+  public double duration();
+
+  public double place();
+
+  public List<User> attendees();
+
+  public User host();
+
+  public Recipe recipe();
 }
