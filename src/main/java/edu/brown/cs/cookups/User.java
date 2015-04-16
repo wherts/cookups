cb6b3e6b239714fedbf9708a6090352c2afef12e
Grant Gustafson <grant_gustafson@brown.edu>
2015-04-16 17:24:52 -0400

    updated person

--- src/main/java/edu/brown/cs/cookups/User.java
+++ src/main/java/edu/brown/cs/cookups/User.java
@@ -1,51 +1,55 @@
 package edu.brown.cs.cookups;
 
+import java.util.List;
 
+import edu.brown.cs.cookups.Ingredient;
+import edu.brown.cs.cookups.Person;
+import edu.brown.cs.cookups.Recipe;
 
-public class User implements Person{
-	
-	private String name;
-	private String id;
-	private List<Ingredient> ingredients;
-	
-	public User (String name, String id, List<Ingredient> ingredients) {
-		this.name = name;
-		this.id = id;
-		this.ingredients = ingredients;
-	}
-	
-	@Override
-	public String name() {
-		return this.name;
-	}
+public class User implements Person {
 
-	@Override
-	public String id() {
-		return this.id;
-	}
+  private String name;
+  private String id;
+  private List<Ingredient> ingredients;
 
-	@Override
-	public List<Ingredient> ingredients() {
-		return ingredients;
-	}
+  public User(String name, String id,
+      List<Ingredient> ingredients) {
+    this.name = name;
+    this.id = id;
+    this.ingredients = ingredients;
+  }
 
-	@Override
-	public Double latitude() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public String name() {
+    return this.name;
+  }
 
-	@Override
-	public Double longitude() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public String id() {
+    return this.id;
+  }
 
-	@Override
-	public List<Recipe> favoriteRecipes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public List<Ingredient> ingredients() {
+    return ingredients;
+  }
 
+  @Override
+  public Double latitude() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Double longitude() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Recipe> favoriteRecipes() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
