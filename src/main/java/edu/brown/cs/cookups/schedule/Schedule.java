735e928243194a0eb141dcb99bed04945454eb47
Wesley Herts <wesley_herts@brown.edu>
2015-04-24 16:37:12 -0400

    finished scheduling testing

--- src/main/java/edu/brown/cs/cookups/schedule/Schedule.java
+++ src/main/java/edu/brown/cs/cookups/schedule/Schedule.java
@@ -1,8 +1,10 @@
 package edu.brown.cs.cookups.schedule;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
 /**
  * This class represents a Schedule
  * object for cookups. It includes
@@ -11,8 +13,7 @@ import java.time.LocalTime;
  *
  */
 public class Schedule {
-  private LocalDate date;
-  private LocalTime start, end;
+  private LocalDateTime start, end;
   private LatLong location;
 
   /**
@@ -21,8 +22,8 @@ public class Schedule {
    * @param location of event
    */
   public Schedule(LocalDateTime datetime, LatLong loc) {
-    this.date = datetime.toLocalDate();
-    this.start = datetime.toLocalTime();
+    assert (datetime != null && loc != null);
+    this.start = datetime;
     this.location = loc;
   }
 
@@ -33,8 +34,8 @@ public class Schedule {
    */
   public LocalDate changeDate(LocalDate lD) {
     assert (lD != null);
-    LocalDate ret = date;
-    this.date = lD;
+    LocalDate ret = start.toLocalDate();
+    this.start = LocalDateTime.of(lD, start.toLocalTime());
     return ret;
   }
 
@@ -43,9 +44,9 @@ public class Schedule {
    * @return date
    */
   public LocalDate date() {
-    return LocalDate.of(date.getYear(),
-                        date.getMonthValue(),
-                        date.getDayOfMonth());
+    return LocalDate.of(start.getYear(),
+                        start.getMonthValue(),
+                        start.getDayOfMonth());
   }
 
   /**
@@ -55,8 +56,8 @@ public class Schedule {
    */
   public LocalTime changeTime(LocalTime lT) {
     assert (lT != null);
-    LocalTime ret = start;
-    this.start = lT;
+    LocalTime ret = start.toLocalTime();
+    this.start = LocalDateTime.of(start.toLocalDate(), lT);
     return ret;
   }
 
@@ -72,8 +73,22 @@ public class Schedule {
    * Setter for an end time.
    * @param lT end time
    */
-  public void setEndTime(LocalTime lT) {
+  public void setEndTime(LocalDateTime lT) {
     assert (lT != null);
+    Period dayDifference = Period.between(start.toLocalDate(), lT.toLocalDate());
+    if (dayDifference.getYears() < 0
+        || dayDifference.getMonths() < 0
+        || dayDifference.getDays() < 0) {
+      throw new IllegalArgumentException("ERROR: New end date is before the start date");
+    }
+    if (dayDifference.getYears() == 0
+        && dayDifference.getMonths() == 0
+        && dayDifference.getDays() == 0) { //same day, must check time
+      Duration timeDifference = Duration.between(start.toLocalTime(), lT.toLocalTime());
+      if (timeDifference.isNegative()) {
+        throw new IllegalArgumentException("ERROR: New end time is before the start time");
+      }
+    }
     this.end = lT;
   }
 
@@ -82,10 +97,25 @@ public class Schedule {
    * @return null if no end time is
    * set.
    */
-  public LocalTime end() {
+  public LocalTime endTime() {
     LocalTime ret = null;
     if (end != null) {
       ret = LocalTime.of(end.getHour(), end.getMinute());
+    }
+    return ret;
+  }
+
+  /**
+   * Accessor for end date.
+   * @return null if no end date
+   * is set
+   */
+  public LocalDate endDate() {
+    LocalDate ret = null;
+    if (end != null) {
+      ret = LocalDate.of(end.getYear(),
+                         end.getMonthValue(),
+                         end.getDayOfMonth());
     }
     return ret;
   }
@@ -116,7 +146,7 @@ public class Schedule {
    */
   @Override
   public int hashCode() {
-    return date.hashCode() + start.hashCode();
+    return start.hashCode();
   }
 
   /**
@@ -126,8 +156,7 @@ public class Schedule {
    */
   @Override
   public String toString() {
-    return date.toString() + ", "
-           + start.toString();
+    return start.toString() + ", " + location.toString();
   }
 
   /**
@@ -145,8 +174,8 @@ public class Schedule {
       return false;
     }
     Schedule s = (Schedule) o;
-    return date.equals(s.date())
-           && start.equals(s.start())
+    return this.date().equals(s.date())
+           && this.start().equals(s.start())
            && location.equals(s.location());
   }
 }
